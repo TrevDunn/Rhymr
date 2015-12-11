42a1486fcede05f7169c182743cfbc58a5d748d5
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-12-11 15:31:28 -0500

    all routes working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:email])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect_to users_path
+			redirect_to limericks_path
 		else
 			redirect_to login_path
 		end
 	end
 	def destroy
 		session[:user_id] = nil
-		redirect_to user_path
+		redirect_to limericks_path
 	end
 end
